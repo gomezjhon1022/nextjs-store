5fc445b75d1dff7703f558f6fae96c64b515cc8a
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-01-02 18:28:34 +0100

    Add: Styles to component Description

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,20 @@
+import styles from './Description.module.sass'
+import Image from "next/image"
+import { PLACEHOLDER_IMAGE } from './blurImage'
+
 export const Description = () => {
   return (
-    <section>
-      <h1>Description</h1>
+    <section className={styles.Description}>
+      <div className={styles.Description__imageContainer}>
+        <Image
+          src="/images/description.jpeg" alt="products marketplace"
+          fill
+          placeholder='blur'
+          blurDataURL={PLACEHOLDER_IMAGE}
+        />
+      </div>
+      <h2 className={styles.Description__text}>Make tomorrow happen today</h2>
+      <p>Digital Destiny Store: Explore the latest in cutting-edge gadgets and gear. Embrace innovation, stay at the forefront of technology, and redefine your digital experience with us</p>
     </section>
   )
 }
