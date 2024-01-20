33d607cb5bcccdbe27bb0669d7bf4620f700d338
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-01-19 19:51:24 -0500

    Feat: Process of sign-up with graphql

--- src/components/signup/NewAccountForm/NewAccountForm.tsx
+++ src/components/signup/NewAccountForm/NewAccountForm.tsx
@@ -8,25 +8,25 @@ export const NewAccountForm = () => {
   const [errors, setErrors] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
-    handleCreateUser(formData)
+    await handleCreateUser(formData)
   }
 
   return (
     <div className={styles.NewAccountForm}  >
       <h1 className={styles.NewAccountForm__title}>New Account</h1>
       <form className={styles.NewAccountForm__form} onSubmit={handleSubmit}>
-        <input type="text" name="first_name" placeholder="name" disabled={loading} />
-        <input type="text" name="last_name" placeholder="last name" disabled={loading} />
+        <input type="text" name="firstName" placeholder="name" disabled={loading} />
+        <input type="text" name="lastName" placeholder="last name" disabled={loading} />
         <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
         <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
         <input type="password" name="password" placeholder="password" disabled={loading} />
         <input type="password" name="password_confirmation" placeholder="re-type password" disabled={loading} />
         <input type="submit" name="submit" value="Create account" disabled={loading} />
       </form>
-      {errors.length > 0 && 
+      {errors.length > 0 &&
         <div>
           {errors.map((error, index) => {
             return <p key={index} className={styles.NewAccountForm__error}>{error}</p>
