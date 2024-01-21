658820cff26266a7ae998e67cbcaaf1bfc0471ee
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-01-21 16:02:36 -0500

    Feat: Add cart buy

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import { useState } from "react"
 import { FaShoppingCart } from "react-icons/fa"
+import { useShoppingCart } from "app/hooks/useShoppingCart"
 import styles from './ShoppingCart.module.sass'
+
 export const ShoppingCart = () => {
-  const [countItems, setCountItems] = useState(0);
+  const { cart } = useShoppingCart()
   return (
     <button className={styles.ShoppingCart}>
       <span className={styles.ShoppingCart__counter}>
-        {countItems}
+        {cart.length}
       </span>
       <FaShoppingCart />
     </button>
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,22 @@
 interface ErrorPageProps {
   error: Error;
-  reset: ()=> void;
+  reset: () => void;
+}
+
+type ProductType = {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  quantity: number;
+  handle: string;
+  tags: string;
+};
+
+type CartItem = {
+  title: string;
+  price: number;
+  quantity: number;
+  id: string;
 }
