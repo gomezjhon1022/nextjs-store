e81dc094b6cb79af668140c2a2761dddc33553f5
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-01-24 20:27:21 -0500

    Feat: Parallel routin

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,14 +1,9 @@
-import { getCustomerOrders } from "app/services/shopify/graphql/customer";
-import { validateAccessToken } from "app/utils/auth/validateAccessToken";
 
 export default async function MyAccountPage() {
-  const customer = await validateAccessToken();
-  const ordersInfo = await getCustomerOrders();
 
   return (
     <div>
-      <h1>Bienvenido {customer.name}</h1>
-      <p>email: {customer.email}</p>
+      <h1>My Acount</h1>
     </div>
   );
 }
